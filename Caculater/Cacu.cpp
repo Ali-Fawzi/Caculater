1a1505aeb3b097409bf12bb39c1a109272be6a3d
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-05-29 17:04:47 +0300

    Update Cacu.cpp

--- Caculater/Cacu.cpp
+++ Caculater/Cacu.cpp
@@ -7,23 +7,23 @@ int main() {
     double x, y;
     char op;
     char opt;
-    system("Color 3");
-    cout << "welcome to the caculator ^_^ " << endl;
+    system("Color 3"); // set colour to blue
+    cout << "welcome to the caculator ^_^ " << endl;      //shows all this to the user
         cout << "===============================\n";
         cout << "> press 1 for normal caculator. \n";
         cout << "> press 2 for finding the square root. \n";
         cout << "> press 3 for powering up numbers. \n";
         cout << "> press any Button to EXIT.\n ";
-        cin  >> opt;
-        switch (opt) {
+        cin  >> opt;              //get input from the user
+        switch (opt) {            //switching options
         case '1':
-            cout << "Enter a num1 :" << endl;
-            cin  >> x;
+            cout << "Enter a num1 :" << endl; 
+            cin  >> x;            //geting input as num 1
             cout << "Enter OP : " << endl;
-            cin >> op;
+            cin >> op;            //geting operator for the operation
             cout << "Enter a num2 :" << endl;
-            cin  >> y;
-            switch (op) {
+            cin  >> y;            //geting input as num 2
+            switch (op) {         //switching operations
             case '+':
                 cout << x + y << endl;
                 break;
@@ -37,20 +37,20 @@ int main() {
                 cout << x / y << endl;
                 break;
             default:
-                cout << "invalid op" << endl;
+                cout << "invalid op" << endl; //activated when inputing wrong operator
                 break;
             }   break;
                 case '2':
                 cout << "enter number that you want it square root " << endl;
-                cin  >> x;
+                cin  >> x;               //geting input for the square root
                 cout << sqrt(x) << endl;
                 break;
             case '3':
                 cout << "enter number\n";
-                cin >> x;
+                cin >> x; //input as the number
                 cout << "enter the power of number\n";
-                cin >> y;
-                cout << pow(x, y) << endl;
+                cin >> y; //input as the power of it
+                cout << pow(x, y) << endl; // showing the result
                 break;
         }
         return 0;
