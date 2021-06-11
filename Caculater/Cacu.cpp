d77b02f75e040c7457d9e7338d602348a1afc94a
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-11 23:06:06 +0300

    Update Cacu.cpp

--- Caculater/Cacu.cpp
+++ Caculater/Cacu.cpp
@@ -52,7 +52,7 @@ void LOOP() {
             std::cout << "enter number\n";
             std::cin >> x;                              //input as the number
             std::cout << "enter the power of number\n";
-            std::cin >> y;                              //input as the power of it
+            std::cin >> y;//input as the power of it
             std::cout << pow(x, y) << std::endl;// showing the result
             break;
         case 4:
